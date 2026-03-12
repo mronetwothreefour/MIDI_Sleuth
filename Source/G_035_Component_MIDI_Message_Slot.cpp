3a1901279a525931cc2ccf703808fa1d5ae409ac
mronetwothreefour <41154573+mronetwothreefour@users.noreply.github.com>
2026-03-11 21:07:22 -0600

    MS: Message slots
    Files left out of previous commit.

---
+++ Source/G_035_Component_MIDI_Message_Slot.cpp
@@ -0,0 +1,26 @@
+#include "G_035_Component_MIDI_Message_Slot.h"
+
+Component_MIDI_Message_Slot::Component_MIDI_Message_Slot(int slot_num, Data_Hub* hub, Component_MIDI_Devices* devices) :
+	Data_User{ hub },
+	slot_num{ slot_num },
+	devices{ devices },
+	btn_edit{ "Edit" },
+	btn_send{ "Send" }
+{
+	btn_edit.setBounds(XYWH::msg_slot_btn_edit);
+	btn_edit.addShortcut(KeyPress{ 0x31 + slot_num, ModifierKeys::ctrlAltCommandModifiers, 0 });
+	addAndMakeVisible(btn_edit);
+
+	btn_send.setBounds(XYWH::msg_slot_btn_send);
+	btn_send.onClick = [this, devices, slot_num] { devices->transmit_stored_message(slot_num); };
+	btn_send.addShortcut(KeyPress{ 0x31 + slot_num, ModifierKeys::ctrlModifier, 0 });
+	addAndMakeVisible(btn_send);
+
+	setSize(XYWH::msg_slot_w, XYWH::msg_slot_h);
+}
+
+void Component_MIDI_Message_Slot::paint(Graphics& g) {
+	g.setColour(COLOR::text);
+	g.setFont(FONT::msg_slot_header);
+	g.drawText("Message Slot " + String{ slot_num + 1 }, XYWH::msg_slot_lbl, Justification::centredTop);
+}
---
+++ Source/G_035_Component_MIDI_Message_Slot.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+#include "D_100_Data_User.h"
+#include "G_020_Component_MIDI_Devices.h"
+
+class Component_MIDI_Message_Slot :
+	public Component,
+	public Data_User
+{
+private: const int slot_num;
+private: Component_MIDI_Devices* devices;
+private: TextButton btn_edit;
+private: TextButton btn_send;
+
+//==============================================================================
+public: Component_MIDI_Message_Slot(int slot_num, Data_Hub* hub, Component_MIDI_Devices* devices);
+
+public: void paint(Graphics& g) override;
+
+//==============================================================================
+private: JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Component_MIDI_Message_Slot)
+};
